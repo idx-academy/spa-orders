33036308b07f709ae5c16f4146c0200fe7bc37db
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-06-19 22:32:43 +0300

    AppTypography

--- code/src/layouts/app-banner/AppBanner.tsx
+++ code/src/layouts/app-banner/AppBanner.tsx
@@ -1,11 +1,10 @@
 import { FC } from "react";
-import { FormattedMessage } from "react-intl";
 
 import Box from "@mui/material/Box";
-import { Button } from "@mui/material";
-import Typography from "@mui/material/Typography";
 
 import PageWrapper from "@/layouts/app-wrapper/PageWrapper";
+import AppTypography from "@/components/app-typography/AppTypography";
+import AppButton from "@/components/app-button/AppButton";
 
 import "@/layouts/app-banner/AppBanner.scss";
 
@@ -15,20 +14,14 @@ const AppBanner: FC = () => {
       <PageWrapper>
         <Box className="banner__container">
           <Box>
-            <Typography>
-              <FormattedMessage id="appBanner.header" />
-            </Typography>
+            <AppTypography variant="h1" translationKey="appBanner.header" />
           </Box>
           <Box className="banner__paragraph">
-            <Typography>
-              <FormattedMessage id="appBanner.paragraphfirst" />
-            </Typography>
-            <Typography>
-              <FormattedMessage id="appBanner.paragraphsecond" />
-            </Typography>
+            <AppTypography translationKey="appBanner.paragraphfirst" />
+            <AppTypography translationKey="appBanner.paragraphsecond" />
           </Box>
           <Box className="banner__button">
-            <Button variant="contained">Shop Now</Button>
+            <AppButton variant="contained">Shop Now</AppButton>
           </Box>
         </Box>
       </PageWrapper>
