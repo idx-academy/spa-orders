54b746e06ad944b2d48cee2955956cdc26dde4cb
dmtrth25 <vodolazhchenko25@gmail.com>
2024-06-20 17:43:40 +0300

    some fix

--- code/src/layouts/app-banner/AppBanner.test.tsx
+++ code/src/layouts/app-banner/AppBanner.test.tsx
@@ -2,8 +2,6 @@ import { render, screen } from "@testing-library/react";
 import AppBanner from "@/layouts/app-banner/AppBanner";
 import I18nProivider from "@/shared/i18n/I18nProvider";
 
-jest.mock("@/assets/images/home-page/hero_section_img.jpg", () => {});
-
 describe("AppBanner", () => {
   test("should render AppBanner component", () => {
     render(
--- code/src/layouts/app-header/components/header/Header.test.tsx
+++ code/src/layouts/app-header/components/header/Header.test.tsx
@@ -1,19 +1,17 @@
+import { FormattedMessage } from "react-intl";
 import { render, screen } from "@testing-library/react";
-import { IntlProvider } from "react-intl";
 import Header from "@/layouts/app-header/components/header/Header";
 
 jest.mock("react-intl", () => ({
-  ...jest.requireActual("react-intl"),
-  FormattedMessage: ({ value }) => <span>{value}</span>
+  FormattedMessage: jest.fn()
 }));
 
 describe("Header Component", () => {
   test("render the logo", () => {
-    render(
-      <IntlProvider locale="en">
-        <Header />
-      </IntlProvider>
-    );
+    (FormattedMessage as jest.Mock).mockReturnValueOnce("Logo");
+
+    render(<Header />);
+
     const logo = screen.getByAltText("Logo");
     expect(logo).toBeInTheDocument();
   });
