fe29093c6de7176b1ab51227aad252e7deffdf8d
Valentin Kolev Dimitrov <v.dimitrov_1994@abv.bg>
2024-06-20 14:55:03 +0300

    Subintro section

---
+++ code/src/components/subintro/Subintro.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Subintro from "../subintro/Subintro";
+import {subintroData} from "../subintro/subintroData"
+jest.mock("@/assets/images/subintro/moped-icon.jpg", () => {});
+jest.mock("@/assets/images/subintro/box-icon.jpg", () => {});
+jest.mock("@/assets/images/subintro/percent-icon.jpg", () => {});
+jest.mock("@/assets/images/subintro/clock-icon.jpg", () => {});
+
+describe("Subintro Component", () => {
+    test("render number length of subintro elements", () => {
+      render(<Subintro />);
+      const subintroItems = screen.getAllByTestId("subintro-item");
+      expect(subintroItems).toHaveLength(subintroData.length);
+    });
+  });
