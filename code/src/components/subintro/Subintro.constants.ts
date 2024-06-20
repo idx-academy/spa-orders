0d13715814dbaaedc8040c536c9e67d75cbdfb44
Valentin Kolev Dimitrov <v.dimitrov_1994@abv.bg>
2024-06-20 20:13:59 +0300

    Formating and className fixes

--- code/src/components/subintro/Subintro.constants.ts
+++ code/src/components/subintro/Subintro.constants.ts
@@ -3,28 +3,27 @@ import boxIcon from "@/assets/images/subintro/box-icon.jpg";
 import percentIcon from "@/assets/images/subintro/percent-icon.jpg";
 import clockIcon from "@/assets/images/subintro/clock-icon.jpg";
 
-
 const subintroData = [
-    {
-        id: 1,
-        content: "subintro.firstParagraph",
-        icon: mopedIcon
-    },
-    {
-        id: 2,
-        content: "subintro.secondParagraph",
-        icon: boxIcon
-    },
-    {
-        id: 3,
-        content: "subintro.thirdparagraph",
-        icon: percentIcon
-    },
-    {
-        id: 4,
-        content: "subintro.fourthParagraph",
-        icon: clockIcon
-    }
+  {
+    id: 1,
+    content: "subintro.firstParagraph",
+    icon: mopedIcon
+  },
+  {
+    id: 2,
+    content: "subintro.secondParagraph",
+    icon: boxIcon
+  },
+  {
+    id: 3,
+    content: "subintro.thirdparagraph",
+    icon: percentIcon
+  },
+  {
+    id: 4,
+    content: "subintro.fourthParagraph",
+    icon: clockIcon
+  }
 ];
 
-export default  subintroData;
+export default subintroData;
--- code/src/components/subintro/Subintro.tsx
+++ code/src/components/subintro/Subintro.tsx
@@ -5,14 +5,19 @@ import PageWrapper from "@/layouts/app-wrapper/PageWrapper";
 import Box from "@mui/material/Box";
 
 const Subintro = () => {
-    return (
-        <PageWrapper>
-            <Box className="spa-subintro">
-                {subintroData.map(element => <SubintroElement icon={element.icon} key={element.id}
-                    content={element.content} />)}
-            </Box>
-        </PageWrapper>
-    )
-}
+  return (
+    <PageWrapper>
+      <Box className="spa-subintro">
+        {subintroData.map((element) => (
+          <SubintroElement
+            icon={element.icon}
+            key={element.id}
+            content={element.content}
+          />
+        ))}
+      </Box>
+    </PageWrapper>
+  );
+};
 
 export default Subintro;
