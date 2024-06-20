49a3b0434730fc766d555a27ebc8e92fa2d718a7
Valentin Kolev Dimitrov <v.dimitrov_1994@abv.bg>
2024-06-20 20:13:59 +0300

    Formating and className fixes

--- code/src/components/subintroElement/SubintroElement.tsx
+++ code/src/components/subintroElement/SubintroElement.tsx
@@ -2,17 +2,21 @@ import Box from "@mui/material/Box";
 import AppTypography from "@/components/app-typography/AppTypography";
 
 type SubintroElement = {
-    content: string;
-    icon: string;
-}
+  content: string;
+  icon: string;
+};
 
 const SubintroElement = ({ content, icon }: SubintroElement) => {
-    return (
-        <Box className="spa-subintro_element" data-testid="spa-subintro-item">
-            <img className="spa-subintro_icon" src={icon} />
-            <AppTypography translationKey={content} variant="subtitle2" className="spa-subintro_content" />
-        </Box>
-    )
+  return (
+    <Box className="spa-subintro__element" data-testid="spa-subintro-item">
+      <img className="spa-subintro__icon" src={icon} />
+      <AppTypography
+        translationKey={content}
+        variant="subtitle2"
+        className="spa-subintro__content"
+      />
+    </Box>
+  );
 };
 
 export default SubintroElement;
