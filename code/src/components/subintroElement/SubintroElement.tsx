78bdeee5fe223fbed06903f4eae6d14aed8fad42
Valentin Kolev Dimitrov <v.dimitrov_1994@abv.bg>
2024-06-20 19:24:34 +0300

    Added AppTypography components and translations

--- code/src/components/subintroElement/SubintroElement.tsx
+++ code/src/components/subintroElement/SubintroElement.tsx
@@ -1,14 +1,16 @@
 import Box from "@mui/material/Box";
+import AppTypography from "@/components/app-typography/AppTypography";
 
 type SubintroElement = {
     content: string;
     icon: string;
 }
+
 const SubintroElement = ({ content, icon }: SubintroElement) => {
     return (
         <Box className="spa-subintro_element" data-testid="spa-subintro-item">
             <img className="spa-subintro_icon" src={icon} />
-            <p className="spa-subintro_content">{content}</p>
+            <AppTypography translationKey={content} variant="subtitle2" className="spa-subintro_content" />
         </Box>
     )
 };
