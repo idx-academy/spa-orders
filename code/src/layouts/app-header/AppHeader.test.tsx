133bf1ebc2b1d6f9bdcffc95a4fbabdf64eec986
mhevyk <maxxevyk7@gmail.com>
2024-06-21 11:43:16 +0300

    Remove unused mocks from tests after adding global translation mock

--- code/src/layouts/app-header/AppHeader.test.tsx
+++ code/src/layouts/app-header/AppHeader.test.tsx
@@ -1,12 +1,7 @@
 import { screen, render } from "@testing-library/react";
 import AppHeader from "@/layouts/app-header/AppHeader";
 
-jest.mock("@/layouts/app-header/components/header/Header", () => ({
-  __esModule: true,
-  default: () => "Header"
-}));
-
 test("renders Header and Categories components", () => {
   render(<AppHeader />);
-  expect(screen.getByText("Header")).toBeInTheDocument();
+  expect(screen.getByText("login.label")).toBeInTheDocument();
 });
