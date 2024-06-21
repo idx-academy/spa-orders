71e05834733ccba9e4e5110add8eadf530630db5
mhevyk <maxxevyk7@gmail.com>
2024-06-21 20:58:43 +0300

    Add alias

--- code/src/components/app-icon-button/AppIconButton.test.tsx
+++ code/src/components/app-icon-button/AppIconButton.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import AppIconButton from "./AppIconButton";
+import AppIconButton from "@/components/app-icon-button/AppIconButton";
 
 describe("AppIconButton", () => {
   test("renders correctly", () => {
