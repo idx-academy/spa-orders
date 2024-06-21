360910d33c5b865c3477584421054b340290020b
mhevyk <maxxevyk7@gmail.com>
2024-06-21 08:10:27 +0300

    Refactor test setup, add mock for FormattedMessage component

--- code/src/layouts/app-banner/AppBanner.test.tsx
+++ code/src/layouts/app-banner/AppBanner.test.tsx
@@ -1,17 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import AppBanner from "@/layouts/app-banner/AppBanner";
-import I18nProivider from "@/shared/i18n/I18nProvider";
 
 describe("AppBanner", () => {
   test("should render AppBanner component", () => {
-    render(
-      <I18nProivider>
-        <AppBanner />
-      </I18nProivider>
-    );
-    const headerText = screen.getByText(
-      /Incredible Prices on All Your Favorite Items/i
-    );
+    render(<AppBanner />);
+    const headerText = screen.getByText("appBanner.header");
     expect(headerText).toBeInTheDocument();
   });
 });
