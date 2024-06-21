c02396698cb31f45cdfb73b668c5a07e2ff4dedc
Valentin Kolev Dimitrov <v.dimitrov_1994@abv.bg>
2024-06-21 11:06:12 +0300

    Fix

--- code/src/components/subintro/Subintro.test.tsx
+++ code/src/components/subintro/Subintro.test.tsx
@@ -1,10 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Subintro from "@/components/subintro/Subintro";
 import subintroData from "@/components/subintro/Subintro.constants";
-jest.mock("@/assets/images/subintro/moped-icon.jpg", () => {});
-jest.mock("@/assets/images/subintro/box-icon.jpg", () => {});
-jest.mock("@/assets/images/subintro/percent-icon.jpg", () => {});
-jest.mock("@/assets/images/subintro/clock-icon.jpg", () => {});
+
 
 describe("Subintro Component", () => {
   test("render all subintro elements correctly", () => {
