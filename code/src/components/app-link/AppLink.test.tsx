a4815ac468c5713961a4b470d72d385b2634be5d
mhevyk <maxxevyk7@gmail.com>
2024-06-22 14:04:16 +0300

    Fix test

--- code/src/components/app-link/AppLink.test.tsx
+++ code/src/components/app-link/AppLink.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import AppLink from "@/components/app-link/AppLink";
+import { BrowserRouter } from "react-router-dom";
 
 describe("AppLink", () => {
   test("renders correctly", () => {
-    render(<AppLink to="/">Test link</AppLink>);
+    render(
+      <BrowserRouter>
+        <AppLink to="/">Test link</AppLink>
+      </BrowserRouter>
+    );
     const link = screen.getByRole("link");
     expect(link).toBeInTheDocument();
   });
