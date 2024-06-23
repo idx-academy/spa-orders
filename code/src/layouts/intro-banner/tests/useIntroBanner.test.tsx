90bab5aab4be5b80152cb59ab74a67629ceeeb7e
Dinviz <eugene.hetsyanyn@gmail.com>
2024-06-23 22:17:44 +0300

    added test

---
+++ code/src/layouts/intro-banner/tests/useIntroBanner.test.tsx
@@ -0,0 +1,20 @@
+import { renderHook, act } from "@testing-library/react";
+import useIntroBanner from "@/layouts/intro-banner/useIntroBanner";
+import { introBannerImages } from "@/layouts/intro-banner/IntroBanner.constants";
+
+describe("useIntroBanner", () => {
+  test("renders useIntroBanner correctly with 100ms delay", async () => {
+    const timeTick = 100;
+    const timer = jest.useFakeTimers();
+
+    const { result } = renderHook(() =>
+      useIntroBanner(introBannerImages, timeTick)
+    );
+
+    act(() => {
+      timer.advanceTimersByTime(timeTick);
+    });
+
+    expect(result.current).toBe(1);
+  });
+});
