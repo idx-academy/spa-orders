92d2e770dd516c4935f4a1f879fde94a68f1f63e
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-06-25 00:43:52 +0300

    Added route for 'shop all' page (#90)
    * Added route for Shop All page

--- code/src/layouts/app-header/components/categories/Categories.constants.ts
+++ code/src/layouts/app-header/components/categories/Categories.constants.ts
@@ -1,1 +1,8 @@
-export const categories = ["Shop All", "Computers", "Tablets", "Mobile"];
+import routes from "@/constants/routes";
+
+export const categories = [
+  { label: "Shop All", href: routes.products.path },
+  { label: "Computers", href: routes.home.path },
+  { label: "Tablets", href: routes.home.path },
+  { label: "Mobile", href: routes.home.path }
+];
--- code/src/layouts/app-header/components/categories/Categories.test.tsx
+++ code/src/layouts/app-header/components/categories/Categories.test.tsx
@@ -1,10 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Categories from "@/layouts/app-header/components/categories/Categories";
 import { categories } from "@/layouts/app-header/components/categories/Categories.constants";
 
-describe("Categories Component", () => {
+describe("Categories component", () => {
   test("render number length of categories", () => {
-    render(<Categories />);
+    render(
+      <BrowserRouter>
+        <Categories />
+      </BrowserRouter>
+    );
 
     const categoryItems = screen.getAllByTestId("menu-item");
     expect(categoryItems).toHaveLength(categories.length);
