d2ce94a6325e3ad2bb1381cf2cca968e6b775cd6
ValKoDim <v.dimitrov_1994@abv.bg>
2024-06-24 16:04:23 +0300

    Fix

--- code/src/layouts/subintro/Subintro.test.tsx
+++ code/src/layouts/subintro/Subintro.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import Subintro from "@/layouts/subintro/Subintro";
-import subintroItem from "@/layouts/subintro/components/SubintroItem";
+import subintroElements from "@/layouts/subintro/Subintro.constants";
 
 describe("Subintro section", () => {
   test("renders four subintro elements correctly", () => {
     render(<Subintro />);
 
     const subintroItems = screen.getAllByTestId("spa-subintro-item");
-    expect(subintroItems).toHaveLength(subintroItem.length);
+    expect(subintroItems).toHaveLength(subintroElements.length);
   });
 });
