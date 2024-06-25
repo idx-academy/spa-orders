cb468fa27dd9da872af46f5337462405b651d1f0
Dinviz <eugene.hetsyanyn@gmail.com>
2024-06-25 12:37:35 +0300

    refactor components according to new PageWrapper and design

--- code/src/layouts/subintro/Subintro.tsx
+++ code/src/layouts/subintro/Subintro.tsx
@@ -1,22 +1,20 @@
 import subintroData from "@/layouts/subintro/Subintro.constants";
 import SubintroItem from "@/layouts/subintro/components/SubintroItem";
-import PageWrapper from "@/layouts/page-wrapper/PageWrapper";
-import "@/layouts/subintro/subintro.scss";
 import AppBox from "@/components/app-box/AppBox";
+
+import "@/layouts/subintro/subintro.scss";
 
 const Subintro = () => {
   return (
-    <PageWrapper>
-      <AppBox className="spa-subintro">
-        {subintroData.map((element) => (
-          <SubintroItem
-            icon={element.icon}
-            key={element.id}
-            content={element.content}
-          />
-        ))}
-      </AppBox>
-    </PageWrapper>
+    <AppBox className="spa-subintro">
+      {subintroData.map((element) => (
+        <SubintroItem
+          icon={element.icon}
+          key={element.id}
+          content={element.content}
+        />
+      ))}
+    </AppBox>
   );
 };
 
