3c6ddc42ea4a3961d22cc338c540f33e4660a34a
mhevyk <maxxevyk7@gmail.com>
2024-06-25 23:33:38 +0300

    Change order of imports

--- code/src/pages/home/components/call-to-action-section/CallToAction.test.tsx
+++ code/src/pages/home/components/call-to-action-section/CallToAction.test.tsx
@@ -1,6 +1,6 @@
+import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
 import CallToActionSection from "@/pages/home/components/call-to-action-section/CallToActionSection";
-import { MemoryRouter } from "react-router-dom";
 
 describe("CallToAction", () => {
   test("renders correctly", () => {
