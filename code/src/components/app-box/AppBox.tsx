1b34a9ab340e8e562c6b2a719a31ac2cf0657a17
mhevyk <maxxevyk7@gmail.com>
2024-06-25 07:57:25 +0300

    Replace unknown on HTMLElement

--- code/src/components/app-box/AppBox.tsx
+++ code/src/components/app-box/AppBox.tsx
@@ -4,7 +4,7 @@ import Box, { BoxProps } from "@mui/material/Box";
 type AppBoxProps<TElement> = BoxProps & HTMLProps<TElement>;
 
 const AppBox = forwardRef(
-  <TElement,>(props: AppBoxProps<TElement>, ref: ForwardedRef<unknown>) => {
+  <TElement,>(props: AppBoxProps<TElement>, ref: ForwardedRef<HTMLElement>) => {
     return <Box ref={ref} {...props} />;
   }
 );
