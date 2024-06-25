8ad2df1de53a350d8f3b71aeb63bf9b425027427
Dinviz <eugene.hetsyanyn@gmail.com>
2024-06-25 10:35:20 +0300

    removed redundant test

--- code/src/components/app-loader/AppLoader.test.tsx
+++ code/src/components/app-loader/AppLoader.test.tsx
@@ -7,12 +7,4 @@ describe("AppLoader", () => {
     const loader = screen.getByRole("progressbar");
     expect(loader).toBeInTheDocument();
   });
-
-  test("renders AppLoader with additional class names", () => {
-    render(<AppLoader className="custom-class" />);
-    const loader = screen.getByRole("progressbar");
-    expect(loader).toBeInTheDocument();
-    expect(loader).toHaveClass("spa-loader");
-    expect(loader).toHaveClass("custom-class");
-  });
 });
