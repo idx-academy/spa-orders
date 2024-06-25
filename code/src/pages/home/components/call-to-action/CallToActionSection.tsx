1e1e34d7224c2e41fb7fd3a3f4abb58b703e3b9b
mhevyk <maxxevyk7@gmail.com>
2024-06-25 23:52:30 +0300

    Replace AppContainer with AppBox

--- code/src/pages/home/components/call-to-action/CallToActionSection.tsx
+++ code/src/pages/home/components/call-to-action/CallToActionSection.tsx
@@ -1,4 +1,4 @@
-import AppContainer from "@/components/app-container/AppContainer";
+import AppBox from "@/components/app-box/AppBox";
 import CallToActionItem from "@/layouts/call-to-action-item/CallToActionItem";
 
 import callToActionItems from "@/pages/home/components/call-to-action/CallToAction.constants";
@@ -7,11 +7,11 @@ import "@/pages/home/components/call-to-action/CallToActionSection.scss";
 
 const CallToActionSection = () => {
   return (
-    <AppContainer maxWidth="xl" className="call-to-action">
+    <AppBox className="call-to-action">
       {callToActionItems.map((itemData) => (
         <CallToActionItem key={itemData.titleTranslationKey} {...itemData} />
       ))}
-    </AppContainer>
+    </AppBox>
   );
 };
 
