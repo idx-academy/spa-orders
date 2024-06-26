a6a2569fb7db605a95a4696c502ad8341a66a622
dmtrth25 <vodolazhchenko25@gmail.com>
2024-06-26 17:17:37 +0300

    removed testing import

--- code/src/components/product-card/ProductCard.test.tsx
+++ code/src/components/product-card/ProductCard.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import "@testing-library/jest-dom";
 import ProductCard from "@/components/product-card/ProductCard";
 import { Product } from "@/types/product.types";
 
