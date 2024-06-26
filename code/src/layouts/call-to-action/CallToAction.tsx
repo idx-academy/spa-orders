2676e718236e6b9d80e8d27b8812ba81d4d2d10b
mhevyk <maxxevyk7@gmail.com>
2024-06-26 12:02:54 +0300

    Fix comments

--- code/src/layouts/call-to-action/CallToAction.tsx
+++ code/src/layouts/call-to-action/CallToAction.tsx
@@ -1,6 +1,5 @@
 import AppBox from "@/components/app-box/AppBox";
 import CallToActionItem from "@/layouts/call-to-action/components/call-to-action-item/CallToActionItem";
-
 import callToActionItems from "@/layouts/call-to-action/CallToAction.constants";
 
 import "@/layouts/call-to-action/CallToAction.scss";
