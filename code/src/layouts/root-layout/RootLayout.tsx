7de8beb515d8866fd3b095c08f352e04474b9562
Heorhii Shvab <georgiy.shvab@gmail.com>
2024-06-27 13:25:10 +0300

    Pushed footer to the bottom of the page (#116)
    * Pushed footer to bottom

--- code/src/layouts/root-layout/RootLayout.tsx
+++ code/src/layouts/root-layout/RootLayout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/layouts/header/Header";
 import Footer from "@/layouts/footer/Footer";
+import AppBox from "@/components/app-box/AppBox";
+
+import "@/layouts/root-layout/RootLayout.scss";
 
 const RootLayout = () => {
   return (
-    <>
+    <AppBox className="root-layout">
       <Header />
-      <Outlet />
+      <AppBox className="root-layout__content">
+        <Outlet />
+      </AppBox>
       <Footer />
-    </>
+    </AppBox>
   );
 };
 
