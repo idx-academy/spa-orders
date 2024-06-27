814e22e92e1dd9058ab1a6c16a7f8dcb02bbecea
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-06-27 10:20:49 +0300

    Formated

--- code/src/layouts/root-layout/RootLayout.tsx
+++ code/src/layouts/root-layout/RootLayout.tsx
@@ -1,20 +1,20 @@
-import { Outlet } from 'react-router-dom'
-import Header from '@/layouts/header/Header'
-import Footer from '@/layouts/footer/Footer'
-import AppBox from '@/components/app-box/AppBox'
+import { Outlet } from "react-router-dom";
+import Header from "@/layouts/header/Header";
+import Footer from "@/layouts/footer/Footer";
+import AppBox from "@/components/app-box/AppBox";
 
-import '@/layouts/root-layout/RootLayout.scss'
+import "@/layouts/root-layout/RootLayout.scss";
 
 const RootLayout = () => {
   return (
-    <AppBox className='root-layout'>
+    <AppBox className="root-layout">
       <Header />
-      <AppBox className='root-layout__content'>
+      <AppBox className="root-layout__content">
         <Outlet />
       </AppBox>
       <Footer />
     </AppBox>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
