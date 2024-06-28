fe12eb43c59dc924ed19bdee5ae0b007be38ed5d
Dinviz <eugene.hetsyanyn@gmail.com>
2024-06-28 12:33:20 +0300

    refactored test

--- code/src/components/app-accordion/app-accorion-container/AppAccordionContainer.test.tsx
+++ code/src/components/app-accordion/app-accorion-container/AppAccordionContainer.test.tsx
@@ -2,24 +2,14 @@ import { render, screen } from "@testing-library/react";
 import AppAccordionContainer from "@/components/app-accordion/app-accorion-container/AppAccordionContainer";
 
 describe("AppAccordionContainer", () => {
-  test("renders AppAccordionContainer with custom class", () => {
-    render(
-      <AppAccordionContainer>
-        <div>Accordion Content</div>
-      </AppAccordionContainer>
-    );
-    const accordionElement = screen.getByText("Accordion Content");
-    expect(accordionElement).toBeInTheDocument();
-  });
-
-  test("applies expanded class when expanded", () => {
+  test("renders AppAccordionContainer expanded", () => {
     render(
       <AppAccordionContainer expanded>
-        <div>Accordion Content</div>
+        <div>Child</div>
       </AppAccordionContainer>
     );
     const accordionElement = screen
-      .getByText("Accordion Content")
+      .getByText("Child")
       .closest(".spa-accordion-container");
 
     expect(accordionElement).toHaveClass("spa-accordion-container--expanded");
