0029253253863e4e2606a86e0ed2c28d8e516968
mhevyk <maxxevyk7@gmail.com>
2024-06-29 13:56:35 +0300

    Fix error messages and add todo

--- code/src/constants/common.ts
+++ code/src/constants/common.ts
@@ -2,8 +2,9 @@ export const LOCAL_STORAGE_KEYS = {
   userDetails: "spa-user-details"
 } as const;
 
+// @TODO: rewrite with translations
 export const ERROR_MESSAGES_BY_STATUS = {
   400: "Invalid input data. Please check it and try again",
   401: "User is not authorized to perform this action",
-  409: "Conflict occured"
+  409: "Already exists"
 } as const;
