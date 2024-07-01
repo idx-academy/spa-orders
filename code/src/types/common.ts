c91dbc6818c72eacfa848affc4de1b4fdf324238
mhevyk <maxxevyk7@gmail.com>
2024-07-01 12:09:16 +0300

    Add Prettify typescript utility for easier debugging

--- code/src/types/common.ts
+++ code/src/types/common.ts
@@ -1,5 +1,10 @@
 export type TimerId = ReturnType<typeof setTimeout>;
 
+// utility to debug typescript. More: https://www.youtube.com/watch?v=2lCCKiWGlC0
+export type Prettify<T> = {
+  [K in keyof T]: T[K];
+} & {};
+
 export type Pageable = {
   page: number;
   size: number;
