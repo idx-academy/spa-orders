955d45a774f24912e75f6f16ca04526759ca7d56
mhevyk <maxxevyk7@gmail.com>
2024-07-01 08:52:19 +0300

    Add extra check to rtk error middleware to skip showing snackbar when query type is query (GET method)

--- code/src/store/errorMiddleware.ts
+++ code/src/store/errorMiddleware.ts
@@ -14,30 +14,53 @@ type ErrorPayload = {
   data: APIError;
 };
 
-type ErrorActionPayload = PayloadAction<ErrorPayload & SnackbarPayload>;
+type QueryType = "query" | "mutation";
+
+type ErrorActionMeta = {
+  arg: {
+    type: QueryType;
+  };
+};
+
+type ErrorActionPayload = ErrorPayload & SnackbarPayload;
+type ErrorAction = PayloadAction<
+  ErrorActionPayload,
+  string,
+  Partial<ErrorActionMeta>
+>;
 type StatusCode = keyof typeof ERROR_MESSAGES_BY_STATUS;
 
 export const errorMiddleware: Middleware = () => (next) => (action) => {
-  if (isRejectedWithValue(action)) {
-    const typedAction = action as ErrorActionPayload;
-
-    if (typedAction.payload.isSnackbarHidden) {
-      return next(action);
-    }
-
-    const { data } = typedAction.payload;
-    const status = data.status as StatusCode;
-
-    const errorMessage =
-      ERROR_MESSAGES_BY_STATUS[status] ??
-      "Something went wrong. Please try again later";
-
-    store.dispatch(
-      openSnackbarWithTimeout({
-        message: errorMessage
-      })
-    );
+  if (!isRejectedWithValue(action)) {
+    return next(action);
   }
+
+  const typedAction = action as ErrorAction;
+
+  if (typedAction.payload.isSnackbarHidden) {
+    return next(action);
+  }
+
+  // queries are not triggering a snackbar if isSnackbarHidden is not specified
+  if (
+    typedAction.payload.isSnackbarHidden === undefined &&
+    typedAction.meta?.arg?.type === "query"
+  ) {
+    return next(action);
+  }
+
+  const { data } = typedAction.payload;
+  const status = data?.status as StatusCode;
+
+  const errorMessage =
+    ERROR_MESSAGES_BY_STATUS[status] ??
+    "Something went wrong. Please try again later";
+
+  store.dispatch(
+    openSnackbarWithTimeout({
+      message: errorMessage
+    })
+  );
 
   return next(action);
 };
