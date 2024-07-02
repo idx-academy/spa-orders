458b019d138d398771b1c729b02eaa752a07101a
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-02 22:30:05 +0300

    added ref to appInput

--- code/src/components/app-input/AppInput.tsx
+++ code/src/components/app-input/AppInput.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from "react";
 import TextField from "@mui/material/TextField";
 import AppBox from "@/components/app-box/AppBox";
 import { AppInputProps } from "@/components/app-input/AppInput.types";
@@ -5,21 +6,24 @@ import AppTypography from "@/components/app-typography/AppTypography";
 
 import "@/components/app-input/AppInput.scss";
 
-const AppInput = ({ labelTranslationKey, ...props }: AppInputProps) => {
-  const translatedLabel = labelTranslationKey ? (
-    <AppTypography translationKey={labelTranslationKey} />
-  ) : undefined;
+const AppInput = forwardRef<HTMLInputElement, AppInputProps>(
+  ({ labelTranslationKey, ...props }, ref) => {
+    const translatedLabel = labelTranslationKey ? (
+      <AppTypography translationKey={labelTranslationKey} />
+    ) : undefined;
 
-  return (
-    <AppBox>
-      <TextField
-        size="small"
-        label={translatedLabel}
-        className="spa-app-input__textfield"
-        {...props}
-      />
-    </AppBox>
-  );
-};
+    return (
+      <AppBox>
+        <TextField
+          size="small"
+          label={translatedLabel}
+          className="spa-app-input__textfield"
+          inputRef={ref}
+          {...props}
+        />
+      </AppBox>
+    );
+  }
+);
 
 export default AppInput;
