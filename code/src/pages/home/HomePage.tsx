7ab6009cc954506fa7342aff21dc05967697ce03
mhevyk <maxxevyk7@gmail.com>
2024-07-02 22:09:44 +0300

    Remove redundant from HomePage

--- code/src/pages/home/HomePage.tsx
+++ code/src/pages/home/HomePage.tsx
@@ -4,21 +4,10 @@ import PageWrapper from "@/layouts/page-wrapper/PageWrapper";
 import Subintro from "@/layouts/subintro/Subintro";
 import CategorySection from "@/layouts/category-section/CategorySection";
 import CallToAction from "@/layouts/call-to-action/CallToAction";
-import AppButton from "@/components/app-button/AppButton";
-import useSignIn from "@/hooks/use-sign-in/useSignIn";
 
 const HomePage = () => {
-  const [signIn] = useSignIn();
-
   return (
     <>
-      <AppButton
-        onClick={() => {
-          signIn({ email: "fdg", password: "dfg" });
-        }}
-      >
-        Sign in
-      </AppButton>
       <IntroBanner />
       <PageWrapper>
         <Subintro />
