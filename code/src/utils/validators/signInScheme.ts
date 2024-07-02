77a1da1b497d43d8aa2f04ed4d3522a5a08749a8
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-03 00:24:58 +0300

    updated schemas

--- code/src/utils/validators/signInScheme.ts
+++ code/src/utils/validators/signInScheme.ts
@@ -5,6 +5,13 @@ export const SignInVallidationScheme = z.object({
   password: z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter"
+    })
+    .regex(/\d/, { message: "Password must contain at least one digit" })
+    .regex(/[^A-Za-z0-9]/, {
+      message: "Password must contain at least one special character"
+    })
 });
 
 export type SignInVallidatorType = z.infer<typeof SignInVallidationScheme>;
--- code/src/utils/validators/signUpScheme.ts
+++ code/src/utils/validators/signUpScheme.ts
@@ -2,17 +2,43 @@ import { z } from "zod";
 
 export const SignUpValidationScheme = z
   .object({
-    firstName: z.string().min(1, { message: "First name is required" }),
-    lastName: z.string().min(1, { message: "Last name is required" }),
+    firstName: z
+      .string()
+      .min(2, { message: "First name is too short" })
+      .max(50, { message: "First name is too long" })
+      .regex(/^[a-zA-Zа-яА-ЯёЁіІїЇєЄ'’\-]+$/, {
+        message: "Latin, Cyrillic, ', - allowed"
+      }),
+    lastName: z
+      .string()
+      .min(2, { message: "Last name is too short" })
+      .max(50, { message: "Last name is too long" })
+      .regex(/^[a-zA-Zа-яА-ЯёЁіІїЇєЄ'’\-]+$/, {
+        message: "Latin, Cyrillic, ', - allowed"
+      }),
     email: z
       .string()
       .email({ message: "Please provide a valid email address" }),
     password: z
       .string()
-      .min(8, { message: "Password must be at least 8 characters long" }),
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter"
+      })
+      .regex(/\d/, { message: "Password must contain at least one digit" })
+      .regex(/[^A-Za-z0-9]/, {
+        message: "Password must contain at least one special character"
+      }),
     confirmPassword: z
       .string()
       .min(8, { message: "Password must be at least 8 characters long" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter"
+      })
+      .regex(/\d/, { message: "Password must contain at least one digit" })
+      .regex(/[^A-Za-z0-9]/, {
+        message: "Password must contain at least one special character"
+      })
   })
   .refine(({ password, confirmPassword }) => password === confirmPassword, {
     message: "Passwords don't match",
