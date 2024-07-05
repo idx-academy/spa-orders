14e0f667ffe912a3ccef8f73dee6aadf6a8051c6
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-05 20:28:04 +0300

    added new badge variants

--- code/src/components/app-badge/AppBadge.types.ts
+++ code/src/components/app-badge/AppBadge.types.ts
@@ -1,6 +1,13 @@
 import { BadgeProps } from "@mui/material/Badge";
 
-export type AppBadgeColorVariant = "contained" | "dark" | "danger" | "light";
+export type AppBadgeColorVariant =
+  | "contained"
+  | "dark"
+  | "danger"
+  | "light"
+  | "success"
+  | "pending"
+  | "info";
 
 export type AppBadgeSizeVariant = "small" | "medium" | "large" | "extra-large";
 
