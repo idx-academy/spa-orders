7dd44eff19941595e33944156c2138abae145a1f
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-07-08 17:07:58 +0300

    Added different webpack config files

--- code/config/webpack.config.js
+++ code/config/webpack.config.js
@@ -2,88 +2,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const Dotenv = require("dotenv-webpack");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-
-/*
-module.exports = {
-  entry: path.join(__dirname, "..", "src", "main.tsx"),
-  output: {
-    path:path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    // Combine all generated styles into a single bundle.css file
-    new MiniCssExtractPlugin({ filename: '[name].css' }),
-    // Generate the index.html templates for the development server and SPA+Backend deployment
-    new HtmlWebpackPlugin({
-      inject: false,
-      hash: true,
-      template: path.join(__dirname, "index.html"),
-      filename: 'index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        use: {
-          loader: 'babel-loader',
-        },
-        resolve: {
-          fullySpecified: false,
-        },
-      },
-      {
-        test: /\.tsx?$/,
-        exclude: [/node_modules/, /\.test\.tsx?$/],
-        use: [
-          {
-            loader: 'ts-loader',
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        exclude: /\.raw\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1,
-            },
-          },
-        ],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          {
-            loader: require('mini-css-extract-plugin').loader, // eslint-disable-line global-require
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2,
-            },
-          },
-          'sass-loader',
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
-      '@': path.join(__dirname, '..', 'src'),
-    },
-  },
-  devServer: {
-    port: 3000,
-  },
-};*/
 
 module.exports = {
   entry: path.join(__dirname, "..", "src", "main.tsx"),
@@ -96,12 +15,12 @@ module.exports = {
       template: path.join(__dirname, "index.html")
     }),
     new MiniCssExtractPlugin({ filename: "[name].css", ignoreOrder: true }),
-    new Dotenv({
-      path: path.join(__dirname, "..", ".env.local")
-    }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: path.resolve(__dirname, '../src/assets/icons/favicon.ico'), to: 'favicon.ico' }
+        {
+          from: path.resolve(__dirname, "../src/assets/icons/favicon.ico"),
+          to: "favicon.ico"
+        }
       ]
     })
   ],
---
+++ code/config/webpack.development.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const Dotenv = require("dotenv-webpack");
+const baseConfig = require("./webpack.config");
+
+module.exports = {
+  ...baseConfig,
+  plugins: [
+    ...baseConfig.plugins,
+    new Dotenv({
+      path: path.join(__dirname, "..", ".env.development")
+    })
+  ]
+};
