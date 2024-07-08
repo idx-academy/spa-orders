9176ff1a4cff2d0951fb27f1c622fa1f0178e6d8
mhevyk <maxxevyk7@gmail.com>
2024-07-08 18:50:44 +0300

    Add user roles to cypress constants

--- e2e/cypress/test_module/cypress/fixtures/global-data.ts
+++ e2e/cypress/test_module/cypress/fixtures/global-data.ts
@@ -5,11 +5,17 @@ export const httpStatusCode = {
   gatewayTimeout: 504,
   internalServerError: 500,
   notFound: 404,
-  ok: 200,
+  ok: 200
 } as const;
 
 export const httpMethod = {
   get: "GET",
   post: "POST",
-  put: "PUT",
+  put: "PUT"
+} as const;
+
+export const ROLES = {
+  USER: "ROLE_USER",
+  SHOP_MANAGER: "ROLE_SHOP_MANAGER",
+  ADMIN: "ROLE_ADMIN"
 } as const;
