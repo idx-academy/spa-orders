f10bb12f52b20c7e6617b382c55915a5e255c118
mhevyk <maxxevyk7@gmail.com>
2024-07-10 09:35:38 +0300

    Add tests for modal provider

---
+++ code/src/context/modal/ModalContext.test.tsx
@@ -0,0 +1,75 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+
+import { ModalProvider, useModalContext } from "@/context/modal/ModalContext";
+
+const TestModalContent = () => {
+  return <div>ModalContent</div>;
+};
+
+const ContentPlayground = () => {
+  const { openModal, closeModal, toggleModal } = useModalContext();
+
+  return (
+    <>
+      <button
+        data-testid="openModal"
+        onClick={() => openModal(<TestModalContent />)}
+      >
+        Open modal
+      </button>
+      <button data-testid="closeModal" onClick={closeModal}>
+        Open modal
+      </button>
+      <button
+        data-testid="toggleModal"
+        onClick={() => toggleModal(<TestModalContent />)}
+      >
+        Toggle Modal
+      </button>
+    </>
+  );
+};
+
+describe("ModalContext", () => {
+  describe("ModalProvider", () => {
+    beforeEach(() => {
+      render(
+        <ModalProvider>
+          <ContentPlayground />
+        </ModalProvider>
+      );
+    });
+
+    test("renders content correctly when openModal is called", () => {
+      const openModalButton = screen.getByTestId("openModal");
+      fireEvent.click(openModalButton);
+
+      const modalContent = screen.getByText("ModalContent");
+      expect(modalContent).toBeInTheDocument();
+    });
+
+    test("closes modal correctly when it was previously opened", () => {
+      const openModalButton = screen.getByTestId("openModal");
+      fireEvent.click(openModalButton);
+
+      const closeModalButton = screen.getByTestId("closeModal");
+      fireEvent.click(closeModalButton);
+
+      const modalContent = screen.queryByText("ModalContent");
+      expect(modalContent).not.toBeInTheDocument();
+    });
+
+    test("toggles modal correctly", () => {
+      const toggleModalButton = screen.getByTestId("toggleModal");
+      fireEvent.click(toggleModalButton);
+
+      const modalContentAfterFirstClick = screen.getByText("ModalContent");
+      expect(modalContentAfterFirstClick).toBeInTheDocument();
+
+      fireEvent.click(toggleModalButton);
+
+      const modalContentAfterSecondClick = screen.queryByText("ModalContent");
+      expect(modalContentAfterSecondClick).not.toBeInTheDocument();
+    });
+  });
+});
