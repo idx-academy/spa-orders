24df972b4aab74c1cc596a2a870638153ea3fc97
mhevyk <maxxevyk7@gmail.com>
2024-07-10 19:53:16 +0300

    Add tests to AppButton to cover branch

--- code/src/components/app-button/AppButton.test.tsx
+++ code/src/components/app-button/AppButton.test.tsx
@@ -2,11 +2,28 @@ import { render, screen } from "@testing-library/react";
 
 import AppButton from "@/components/app-button/AppButton";
 
+import renderWithProviders from "@/utils/render-with-providers/renderWithProviders";
+
 describe("AppButton", () => {
   test("renders without crashing", () => {
     render(<AppButton>Test Button</AppButton>);
     const buttonElement = screen.getByRole("button");
     expect(buttonElement).toBeInTheDocument();
+  });
+
+  test("renders as NavLink if isNavLink and to props are passed", () => {
+    const path = "/example";
+    
+    renderWithProviders(
+      <AppButton to={path} isNavLink>
+        Test Button
+      </AppButton>,
+      { initialEntries: [path] }
+    );
+
+    const linkElement = screen.getByRole("link");
+    expect(linkElement).toBeInTheDocument();
+    expect(linkElement).toHaveAttribute("href", path);
   });
 
   test("is disabled when isLoading is true", () => {
