0d942bd4f97765c31e0fa493aac2611ae4f03615
mhevyk <maxxevyk7@gmail.com>
2024-07-10 19:43:46 +0300

    Add test to AppInput to cover branch

--- code/src/components/app-input/AppInput.test.tsx
+++ code/src/components/app-input/AppInput.test.tsx
@@ -3,9 +3,15 @@ import { render, screen } from "@testing-library/react";
 import AppInput from "@/components/app-input/AppInput";
 
 describe("AppInput", () => {
-  test("Should render input field with label text", () => {
+  test("renders input field with label text", () => {
     render(<AppInput labelTranslationKey="Input" />);
     const inputField = screen.getByLabelText("Input");
     expect(inputField).toBeInTheDocument();
+  });
+
+  test("does not render label when labelTranslationKey is not passed", () => {
+    render(<AppInput />);
+    const inputField = screen.queryByLabelText("Input");
+    expect(inputField).not.toBeInTheDocument();
   });
 });
