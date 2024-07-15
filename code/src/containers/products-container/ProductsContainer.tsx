f885546cda49fbfed30ea8dc4847ff87f04de718
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-07-15 16:44:59 +0300

    change skeleton style

--- code/src/containers/products-container/ProductsContainer.tsx
+++ code/src/containers/products-container/ProductsContainer.tsx
@@ -121,7 +121,7 @@ const ProductsContainer = ({
 
   const skeletonCards = repeatComponent(<ProductSkeleton />, loadingItemsCount);
 
-  const isLoadingInProgress = true
+  const isLoadingInProgress = isLoading || isAuthLoading || isCartLoading;
 
   const gridItems = isLoadingInProgress ? skeletonCards : productCards;
 
