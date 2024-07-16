a488a33fdc5c0d0bb77fa8a6e74ddf319dd7f10c
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-16 07:12:02 +0300

    moved order summary to the containers

---
+++ code/src/containers/order-summary/OrderSummary.tsx
@@ -0,0 +1,86 @@
+import AppBox from "@/components/app-box/AppBox";
+import AppButton from "@/components/app-button/AppButton";
+import AppTypography from "@/components/app-typography/AppTypography";
+
+import formatPrice from "@/utils/format-price/formatPrice";
+
+import "@/containers/order-summary/OrderSummary.scss";
+
+type OrderSummaryProps = {
+  totalPrice: number;
+  handleCreateOrder: () => void;
+  isLoading: boolean;
+};
+
+const OrderSummary = ({
+  totalPrice,
+  handleCreateOrder,
+  isLoading
+}: OrderSummaryProps) => {
+  const formattedTotalPrice = formatPrice(totalPrice);
+
+  return (
+    <AppBox className="spa-cart-page__order-summary">
+      <AppTypography
+        className="spa-cart-page__order-summary--label"
+        variant="h1"
+        component="h1"
+        translationKey="orderSummary.label"
+        data-testid="orderSummaryLabel"
+      />
+      <AppBox className="spa-order-summary__details">
+        <AppBox className="spa-order-summary__row">
+          <AppTypography
+            className="spa-order-summary__text"
+            translationKey="subtotal.label"
+            data-testid="subtotalLabel"
+          />
+          <AppTypography
+            className="spa-order-summary__text"
+            variant="subtitle2"
+          >
+            {formattedTotalPrice}
+          </AppTypography>
+        </AppBox>
+        <AppBox className="spa-order-summary__row">
+          <AppTypography
+            translationKey="delivery.label"
+            data-testid="deliveryLabel"
+          />
+          <AppTypography translationKey="free.label" data-testid="freeLabel" />
+        </AppBox>
+        <AppTypography
+          className="spa-order-summary__underline-text"
+          translationKey="country.label"
+          data-testid="countryLabel"
+        />
+        <AppBox className="spa-order-summary__row spa-order-summary__total-line">
+          <AppTypography
+            className="spa-order-summary__total"
+            variant="subtitle2"
+            translationKey="total.label"
+            data-testid="totalLabel"
+          />
+          <AppTypography
+            variant="subtitle2"
+            className="spa-order-summary__total"
+          >
+            {formattedTotalPrice}
+          </AppTypography>
+        </AppBox>
+      </AppBox>
+      <AppButton
+        isLoading={isLoading}
+        onClick={handleCreateOrder}
+        className="spa-order-summary__button"
+        variant="contained"
+        size="medium"
+        data-testid="createOrderButton"
+      >
+        <AppTypography translationKey="createOrder.label" />
+      </AppButton>
+    </AppBox>
+  );
+};
+
+export default OrderSummary;
