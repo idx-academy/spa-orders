126dbd4837f84bd8be77f00e5ca7c43ab1e6a399
mhevyk <maxxevyk7@gmail.com>
2024-07-17 00:39:58 +0300

    Add ReactNode type import

--- code/src/layouts/header/components/header-toolbar/HeaderToolbar.tsx
+++ code/src/layouts/header/components/header-toolbar/HeaderToolbar.tsx
@@ -1,4 +1,6 @@
 // import { useState } from "react";
+import { ReactNode } from "react";
+
 import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
 import ListAltIcon from "@mui/icons-material/ListAlt";
 import LogoutIcon from "@mui/icons-material/Logout";
