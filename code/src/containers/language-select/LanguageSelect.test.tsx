de3130b30e62de03fb70c88c37638fa6ce46f641
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-18 15:37:48 +0300

    added languageSelect

---
+++ code/src/containers/language-select/LanguageSelect.test.tsx
@@ -0,0 +1,30 @@
+import { act, fireEvent, screen } from "@testing-library/react";
+
+import LanguageSelect from "@/containers/language-select/LanguageSelect";
+
+import locales from "@/constants/locales";
+import renderWithProviders from "@/utils/render-with-providers/renderWithProviders";
+
+describe("LanguageSelect", () => {
+  let select: HTMLElement;
+  beforeEach(() => {
+    renderWithProviders(<LanguageSelect />);
+    select = screen.getByRole("combobox");
+  });
+  test("renders without crashing", async () => {
+    expect(select).toBeInTheDocument();
+  });
+
+  test("changes locale on selecting a different language", async () => {
+    fireEvent.mouseDown(select);
+
+    const optionText = locales[1].translationKey;
+    const optionToSelect = screen.getByRole("option", { name: optionText });
+
+    await act(async () => {
+      fireEvent.click(optionToSelect);
+    });
+
+    expect(optionToSelect).toBeInTheDocument();
+  });
+});
---
+++ code/src/containers/language-select/LanguageSelect.tsx
@@ -0,0 +1,49 @@
+import AppMenuItem from "@/components/app-menu-item/AppMenuItem";
+import AppSelect from "@/components/app-select/AppSelect";
+import AppTypography from "@/components/app-typography/AppTypography";
+
+import locales from "@/constants/locales";
+import { useLocaleContext } from "@/context/I18nProvider";
+
+import "@/containers/language-select/LanguageSelect.scss";
+
+const LanguageSelect = () => {
+  const { locale, setLocale } = useLocaleContext();
+
+  const localesItems = locales.map(({ translationKey, key, icon }) => {
+    const handleLocaleChange = () => {
+      setLocale(key);
+    };
+    return (
+      <AppMenuItem
+        key={key}
+        value={key}
+        onClick={handleLocaleChange}
+        className="language-select__item"
+      >
+        <svg className="language-select__item-icon">
+          <use href={icon} />
+        </svg>
+        <AppTypography translationKey={translationKey} />
+      </AppMenuItem>
+    );
+  });
+
+  return (
+    <AppSelect
+      className="language-select__container"
+      labelId="language-select"
+      IconComponent={() => null}
+      inputProps={{
+        className: "language-select"
+      }}
+      defaultValue={locale}
+      value={locale}
+      color="dark"
+    >
+      {localesItems}
+    </AppSelect>
+  );
+};
+
+export default LanguageSelect;
