5c1301b87244eab4b4cb4cd7a0e92752a3657008
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-07-18 13:34:22 +0300

    enhancements

--- code/src/containers/cart-drawer/CartDrawer.test.tsx
+++ code/src/containers/cart-drawer/CartDrawer.test.tsx
@@ -155,12 +155,10 @@ describe("CartDrawer", () => {
 
   test("Should render the cart items with undefined cartItems", () => {
     renderAndMock({ cartItems: undefined });
-  
-    // Verify that the component handles undefined cartItems gracefully
+
     const emptyCartMessageElement = screen.getByText("cart.emptyItem");
     expect(emptyCartMessageElement).toBeInTheDocument();
-  
-    // Ensure that formatPrice is called with 0 when cartItems is undefined
+
     expect(mockFormatPrice).toHaveBeenCalledWith(0);
   });
 });
