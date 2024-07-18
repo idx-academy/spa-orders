e813dce6b7d21f310b3c1fc2145d309ed396062b
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-18 15:37:33 +0300

    added icons, new translations

---
+++ code/src/constants/locales.ts
@@ -0,0 +1,25 @@
+import enIcon from "@/assets/icons/en.svg";
+import ukIcon from "@/assets/icons/uk.svg";
+import { Locale } from "@/context/I18nProvider";
+
+type LocalesType = {
+  key: Locale;
+  translationKey: string;
+  icon?: string;
+};
+
+const locales: LocalesType[] = [
+  {
+    key: "en",
+    translationKey: "language.english",
+    icon: `${enIcon}#en-icon-id`
+  },
+  {
+    key: "uk",
+    translationKey: "language.ukrainian",
+    icon: `${ukIcon}#uk-icon-id`
+
+  }
+];
+
+export default locales;
