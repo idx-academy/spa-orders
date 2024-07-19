8aaad15a845757bf679594a4c75dcc4471eb2c4e
mhevyk <maxxevyk7@gmail.com>
2024-07-19 18:37:21 +0300

    Add time spans constants

---
+++ code/src/constants/timeSpans.ts
@@ -0,0 +1,6 @@
+export const timeSpans = {
+  yesterday: "timespan.yesterday",
+  "last-week": "timespan.last7Days",
+  "last-two-weeks": "timespan.last14Days",
+  "last-month": "timespan.lastMonth"
+} as const;
