97cd1a8e09315db1a700a655f85de631c54504b9
mhevyk <maxxevyk7@gmail.com>
2024-07-19 12:38:36 +0300

    Add classname to icon button

--- code/src/components/app-icon-button/AppIconButton.tsx
+++ code/src/components/app-icon-button/AppIconButton.tsx
@@ -2,8 +2,10 @@ import IconButton from "@mui/material/IconButton";
 
 import { AppIconButtonProps } from "@/components/app-icon-button/AppIconButton.types";
 
-const AppIconButton = (props: AppIconButtonProps) => {
-  return <IconButton {...props} />;
+import cn from "@/utils/cn/cn";
+
+const AppIconButton = ({ className, ...props }: AppIconButtonProps) => {
+  return <IconButton className={cn("spa-icon-button", className)} {...props} />;
 };
 
 export default AppIconButton;
