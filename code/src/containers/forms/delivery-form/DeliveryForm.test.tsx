70d21fbdd092c04d3df4c15e0f77a4ffec87c8d0
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-20 16:07:17 +0300

    created DeliveryForm component with tests

---
+++ code/src/containers/forms/delivery-form/DeliveryForm.test.tsx
@@ -0,0 +1,80 @@
+import { act, fireEvent, screen, waitFor } from "@testing-library/react";
+
+import DeliveryForm from "@/containers/forms/delivery-form/DeliveryForm";
+
+import useCreateOrder from "@/hooks/use-create-order/useCreateOrder";
+import useGetUserDetails from "@/hooks/use-get-user-details/useGetUserDetails";
+import renderWithProviders from "@/utils/render-with-providers/renderWithProviders";
+
+jest.mock("@/hooks/use-create-order/useCreateOrder", () => ({
+  __esModule: true,
+  default: jest.fn()
+}));
+
+jest.mock("@/hooks/use-get-user-details/useGetUserDetails", () => ({
+  __esModule: true,
+  default: jest.fn()
+}));
+
+const mockCreateOrder = jest.fn();
+const mockUserDetails = {
+  id: 123,
+  firstName: "John",
+  lastName: "Doe",
+  email: "john.doe@example.com"
+};
+
+describe("DeliveryForm", () => {
+  beforeEach(() => {
+    (useCreateOrder as jest.Mock).mockReturnValue([
+      mockCreateOrder,
+      { isLoading: false }
+    ]);
+    (useGetUserDetails as jest.Mock).mockReturnValue(mockUserDetails);
+    renderWithProviders(<DeliveryForm totalPrice={100} />);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders the delivery form correctly", () => {
+    const cityInput = screen.getByLabelText(/deliveryForm.city/);
+    const departmentInput = screen.getByLabelText(/deliveryForm.department/);
+    const deliveryMethodSelect = screen.getByLabelText(
+      /deliveryForm.postMethod/
+    );
+    const submitButton = screen.getByRole("button");
+
+    expect(cityInput).toBeInTheDocument();
+    expect(departmentInput).toBeInTheDocument();
+    expect(deliveryMethodSelect).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test("handles input changes and form submission", async () => {
+    const cityInput = screen.getByLabelText(/deliveryForm.city/);
+    const departmentInput = screen.getByLabelText(/deliveryForm.department/);
+
+    const submitButton = screen.getByRole("button");
+
+    await act(() => {
+      fireEvent.change(cityInput, { target: { value: "New York" } });
+      fireEvent.change(departmentInput, { target: { value: "123" } });
+    });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(mockCreateOrder).toHaveBeenCalledWith({
+        city: "New York",
+        deliveryMethod: "NOVA",
+        department: "123",
+        email: mockUserDetails.email,
+        firstName: mockUserDetails.firstName,
+        lastName: mockUserDetails.lastName,
+        userId: mockUserDetails.id
+      });
+    });
+  });
+});
---
+++ code/src/containers/forms/delivery-form/DeliveryForm.tsx
@@ -0,0 +1,121 @@
+import { Controller, useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import OrderSummary from "@/containers/order-summary/OrderSummary";
+
+import AppBox from "@/components/app-box/AppBox";
+import AppInput from "@/components/app-input/AppInput";
+import AppMenuItem from "@/components/app-menu-item/AppMenuItem";
+import AppSelect from "@/components/app-select/AppSelect";
+import AppTypography from "@/components/app-typography/AppTypography";
+
+import { deliveryMethods } from "@/constants/deliveryMethods";
+import useCreateOrder from "@/hooks/use-create-order/useCreateOrder";
+import useGetUserDetails from "@/hooks/use-get-user-details/useGetUserDetails";
+import { PostAddress } from "@/types/delivery.types";
+import { PostAddressValidationScheme } from "@/utils/validators/deliverySheme";
+
+import "@/containers/forms/delivery-form/DeliveryForm.scss";
+
+type DeliveryFormProps = {
+  totalPrice: number;
+};
+
+const DeliveryForm = ({ totalPrice }: DeliveryFormProps) => {
+  const { id, firstName, lastName, email } = useGetUserDetails();
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    formState: { errors, isValid }
+  } = useForm<PostAddress>({
+    resolver: zodResolver(PostAddressValidationScheme),
+    defaultValues: {
+      deliveryMethod: deliveryMethods.NOVA_POST.value
+    }
+  });
+
+  const [createOrder, { isLoading }] = useCreateOrder();
+
+  const onSubmit = (postAddress: PostAddress) => {
+    createOrder({
+      userId: id,
+      firstName,
+      lastName,
+      email,
+      ...postAddress
+    });
+  };
+
+  const deliveryMethodItems = Object.entries(deliveryMethods).map(
+    ([key, { translationKey, value, image }]) => (
+      <AppMenuItem key={key} value={value} className="delivery-form__body-item">
+        <AppBox
+          component="img"
+          src={image}
+          alt="delivery method"
+          className="delivery-form__method-image"
+        />
+        <AppTypography translationKey={translationKey} />
+      </AppMenuItem>
+    )
+  );
+
+  return (
+    <AppBox
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      className="delivery-form"
+    >
+      <AppBox className="delivery-form__body">
+        <AppTypography
+          variant="h3"
+          translationKey="deliveryForm.title"
+          className="delivery-form__body-title"
+        />
+        <AppInput
+          {...register("city")}
+          error={Boolean(errors.city)}
+          data-cy="delivery-city"
+          labelTranslationKey="deliveryForm.city"
+          className="delivery-form__body-input"
+        />
+        <AppInput
+          {...register("department")}
+          error={Boolean(errors.department)}
+          labelTranslationKey="deliveryForm.department"
+          data-cy="delivery-department"
+          className="delivery-form__body-input"
+        />
+        <Controller
+          name="deliveryMethod"
+          control={control}
+          render={({ field }) => (
+            <AppSelect
+              {...field}
+              labelId="delivery-method"
+              label="deliveryForm.postMethod"
+              error={Boolean(errors.deliveryMethod)}
+              data-cy="delivery-method"
+              className="delivery-form__method-select"
+              inputProps={{
+                className: "delivery-form__method-select-input"
+              }}
+            >
+              {deliveryMethodItems}
+            </AppSelect>
+          )}
+        />
+      </AppBox>
+      <OrderSummary
+        isDisabled={!isValid}
+        isLoading={isLoading}
+        totalPrice={totalPrice}
+      />
+    </AppBox>
+  );
+};
+
+export default DeliveryForm;
