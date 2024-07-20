b6e48b580d3ba343d5077bc30ff7467977ee0824
mhevyk <maxxevyk7@gmail.com>
2024-07-20 23:48:24 +0300

    Increased coverage for useRemoveFromCart

--- code/src/hooks/use-remove-from-cart/useRemoveFromCart.test.ts
+++ code/src/hooks/use-remove-from-cart/useRemoveFromCart.test.ts
@@ -1,71 +1,127 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 
 import { useAppDispatch } from "@/hooks/use-redux/useRedux";
-import useRemoveFromCart from "@/hooks/use-remove-from-cart/useRemoveFromCart";
+import useSnackbar from "@/hooks/use-snackbar/useSnackbar";
 import { useRemoveFromCartMutation } from "@/store/api/cartApi";
+import { removeFromLocalCart } from "@/store/slices/localCart";
 import { useUserDetailsSelector } from "@/store/slices/userSlice";
 import { CartItem } from "@/types/cart.types";
 import { UserDetails } from "@/types/user.types";
 
+import useRemoveFromCart from "./useRemoveFromCart";
+
 jest.mock("@/hooks/use-redux/useRedux", () => ({
   __esModule: true,
-  useAppDispatch: jest.fn(() => jest.fn(() => ({ unwrap: () => {} })))
+  useAppDispatch: jest.fn()
 }));
 
 jest.mock("@/hooks/use-snackbar/useSnackbar", () => ({
   __esModule: true,
-  default: jest.fn(() => ({
-    openSnackbarWithTimeout: jest.fn()
-  }))
+  default: jest.fn()
 }));
 
 jest.mock("@/store/api/cartApi", () => ({
-  useRemoveFromCartMutation: jest.fn(() => [jest.fn(), {}])
+  useRemoveFromCartMutation: jest.fn()
 }));
 
 jest.mock("@/store/slices/localCart", () => ({
   __esModule: true,
-  removeFromLocalCart: jest.fn(() => ({}))
+  removeFromLocalCart: jest.fn()
 }));
 
 jest.mock("@/store/slices/userSlice", () => ({
-  useUserDetailsSelector: jest.fn(() => ({}))
+  useUserDetailsSelector: jest.fn()
 }));
 
-const dispatch = jest.fn(() => ({ unwrap: () => {} }));
-const removeFromCartMutation = jest.fn(() => ({ unwrap: () => {} }));
+const successSnackbarConfig = {
+  messageTranslationKey: "cart.itemDeletion.success",
+  variant: "success"
+};
+
+const errorSnackbarConfig = {
+  messageTranslationKey: "cart.itemDeletion.fail",
+  variant: "error"
+};
+
+const mockUnwrap = jest.fn();
+const mockOpenSnackbarWithTimeout = jest.fn();
+
+const mockDispatch = jest.fn(() => ({ unwrap: mockUnwrap }));
+const mockRemoveFromCart = jest.fn(() => ({ unwrap: mockUnwrap }));
 
 const renderAndMock = (user?: Partial<UserDetails>) => {
-  (useAppDispatch as jest.Mock).mockReturnValue(dispatch);
-
+  (useAppDispatch as jest.Mock).mockReturnValue(mockDispatch);
   (useRemoveFromCartMutation as jest.Mock).mockReturnValue([
-    removeFromCartMutation,
+    mockRemoveFromCart,
     {}
   ]);
-
   (useUserDetailsSelector as jest.Mock).mockReturnValue(user);
+  (useSnackbar as jest.Mock).mockReturnValue({
+    openSnackbarWithTimeout: mockOpenSnackbarWithTimeout
+  });
 
   return renderHook(() => useRemoveFromCart());
 };
 
-describe("useAddToCart", () => {
-  afterEach(() => {
+describe("useRemoveFromCart", () => {
+  beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  test("Should call dispatch if there is no user", () => {
+  test("shows success snackbar when removing from local cart is successful", async () => {
     const { result } = renderAndMock();
 
-    result.current[0]({
+    const data = {
       productId: "1"
-    } as unknown as CartItem);
+    } as unknown as CartItem;
 
-    expect(dispatch).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      result.current[0](data);
+    });
+
+    expect(mockDispatch).toHaveBeenCalledWith(removeFromLocalCart(data));
+
+    expect(mockOpenSnackbarWithTimeout).toHaveBeenCalledWith(
+      successSnackbarConfig
+    );
   });
 
-  test("Should call useRemoveFromCartMutation if there is user", () => {
-    renderAndMock({ id: 5 });
+  test("shows success snackbar when removing from server cart successful", async () => {
+    const userId = 5;
+    const productId = 1;
+    const { result } = renderAndMock({ id: userId });
 
-    expect(useRemoveFromCartMutation).toHaveBeenCalledTimes(1);
+    await waitFor(() => {
+      result.current[0]({
+        productId
+      } as unknown as CartItem);
+    });
+
+    expect(mockRemoveFromCart).toHaveBeenCalledWith({
+      userId,
+      productId
+    });
+
+    expect(mockOpenSnackbarWithTimeout).toHaveBeenCalledWith(
+      successSnackbarConfig
+    );
+  });
+
+  test("shows error snackbar when removing from server cart failed", async () => {
+    mockUnwrap.mockImplementationOnce(() => {
+      throw new Error("Error removing from cart");
+    });
+
+    const { result } = renderAndMock({ id: 5 });
+
+    await waitFor(() => {
+      result.current[0]({
+        productId: "1"
+      } as unknown as CartItem);
+    });
+
+    expect(mockOpenSnackbarWithTimeout).toHaveBeenCalledWith(
+      errorSnackbarConfig
+    );
   });
 });
