b15c1415cede8a700ccde1e4ba521ec23ebfd393
mhevyk <maxxevyk7@gmail.com>
2024-07-22 19:30:08 +0300

    Add global useIntl mock

--- code/config/setup-tests.js
+++ code/config/setup-tests.js
@@ -1,7 +1,8 @@
 import React from "react";
+
+import "@testing-library/jest-dom";
 import crypto from "crypto";
 import failOnConsole from "jest-fail-on-console";
-import "@testing-library/jest-dom";
 
 global.React = React;
 
@@ -30,9 +31,16 @@ jest.mock("react-router-dom", () => ({
   })
 }));
 
+const mockTranslationImplementation = jest
+  .fn()
+  .mockImplementation(({ id }) => id);
+
 jest.mock("react-intl", () => ({
   ...jest.requireActual("react-intl"),
-  FormattedMessage: jest.fn().mockImplementation(({ id }) => id)
+  FormattedMessage: mockTranslationImplementation,
+  useIntl: jest.fn().mockImplementation(() => ({
+    formatMessage: mockTranslationImplementation
+  }))
 }));
 
 window.URL.createObjectURL = function () {};
