591dd420daa42917580340ef546d7ef985bd8c56
Dinviz <eugene.hetsyanyn@gmail.com>
2024-07-22 12:55:34 +0300

    refactored app-input

--- code/src/components/app-input/AppInput.tsx
+++ code/src/components/app-input/AppInput.tsx
@@ -2,28 +2,27 @@ import { forwardRef } from "react";
 
 import TextField from "@mui/material/TextField";
 
-import AppBox from "@/components/app-box/AppBox";
 import { AppInputProps } from "@/components/app-input/AppInput.types";
 import AppTypography from "@/components/app-typography/AppTypography";
+
+import cn from "@/utils/cn/cn";
 
 import "@/components/app-input/AppInput.scss";
 
 const AppInput = forwardRef<HTMLInputElement, AppInputProps>(
-  ({ labelTranslationKey, ...props }, ref) => {
+  ({ labelTranslationKey, color = "contained", className, ...props }, ref) => {
     const translatedLabel = labelTranslationKey ? (
       <AppTypography translationKey={labelTranslationKey} />
     ) : undefined;
 
     return (
-      <AppBox>
-        <TextField
-          size="small"
-          label={translatedLabel}
-          className="spa-app-input__textfield"
-          inputRef={ref}
-          {...props}
-        />
-      </AppBox>
+      <TextField
+        size="small"
+        label={translatedLabel}
+        className={cn("spa-input", `spa-input--${color}`, className)}
+        inputRef={ref}
+        {...props}
+      />
     );
   }
 );
--- code/src/components/app-input/AppInput.types.ts
+++ code/src/components/app-input/AppInput.types.ts
@@ -1,5 +1,6 @@
 import { TextFieldProps } from "@mui/material/TextField";
 
-export type AppInputProps = Omit<TextFieldProps, "label"> & {
+export type AppInputProps = Omit<TextFieldProps, "label" | "color"> & {
   labelTranslationKey?: string;
+  color?: "contained" | "success" | "danger" | "light" | "dark";
 };
