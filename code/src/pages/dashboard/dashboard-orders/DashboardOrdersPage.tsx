123d22a0aefefb09d1792612c3c05c984ccde556
mhevyk <maxxevyk7@gmail.com>
2024-07-23 13:36:07 +0300

    Add tests for OrdersTab

--- code/src/pages/dashboard/dashboard-orders/DashboardOrdersPage.tsx
+++ code/src/pages/dashboard/dashboard-orders/DashboardOrdersPage.tsx
@@ -55,7 +55,11 @@ const DashboardOrdersPage = () => {
           variant="h3"
           translationKey="dashboardTabs.orders.title"
         />
-        <AppButton variant="dark" onClick={handleOpenFilterDrawer}>
+        <AppButton
+          variant="dark"
+          onClick={handleOpenFilterDrawer}
+          data-testid="filter-button"
+        >
           {titleTypography}
           <FilterListIcon />
         </AppButton>
