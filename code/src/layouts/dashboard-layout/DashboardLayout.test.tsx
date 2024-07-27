bee0a2ee82a3ca6debc127cd8bcd975619f815f3
mhevyk <maxxevyk7@gmail.com>
2024-07-27 18:01:08 +0300

    Add tests for DashboardLayout.tsx

--- code/src/layouts/dashboard-layout/DashboardLayout.test.tsx
+++ code/src/layouts/dashboard-layout/DashboardLayout.test.tsx
@@ -2,25 +2,58 @@ import { screen } from "@testing-library/react";
 
 import DashboardLayout from "@/layouts/dashboard-layout/DashboardLayout";
 
+import { ROLES } from "@/constants/common";
+import { useUserRoleSelector } from "@/store/slices/userSlice";
+import { UserRole } from "@/types/user.types";
 import renderWithProviders from "@/utils/render-with-providers/renderWithProviders";
 
-jest.mock("react-router-dom", () => ({
-  ...jest.requireActual("react-router-dom"),
-  Outlet: jest.fn(() => <div>Outlet</div>)
+jest.mock(
+  "@/layouts/dashboard-layout/components/dashboard-admin-tab-layout/DashboardAdminLayout",
+  () => ({
+    __esModule: true,
+    default: () => <div>DashboardAdminLayout</div>
+  })
+);
+
+jest.mock(
+  "@/layouts/dashboard-layout/components/dashboard-manager-tab-layout/DashboardManagerLayout",
+  () => ({
+    __esModule: true,
+    default: () => <div>DashboardManagerLayout</div>
+  })
+);
+
+jest.mock("@/store/slices/userSlice", () => ({
+  useUserRoleSelector: jest.fn()
 }));
 
-describe("DashboardTabs", () => {
-  beforeEach(() => {
-    jest.clearAllMocks();
+const renderAndMock = (role?: UserRole) => {
+  (useUserRoleSelector as jest.Mock).mockReturnValue(role);
+  renderWithProviders(<DashboardLayout />);
+};
+
+describe("DashboardLayout", () => {
+  test("renders null if userRole is not admin or shop manager", () => {
+    renderAndMock();
+
+    const adminLayout = screen.queryByText("DashboardAdminLayout");
+    expect(adminLayout).not.toBeInTheDocument();
+
+    const managerLayout = screen.queryByText("DashboardManagerLayout");
+    expect(managerLayout).not.toBeInTheDocument();
   });
 
-  test("Should be rendered correctly", () => {
-    renderWithProviders(<DashboardLayout />);
+  test("renders AdminLayout if userRole admin", () => {
+    renderAndMock(ROLES.ADMIN);
 
-    const buttons = screen.getAllByRole("link");
-    const outletElement = screen.getByText("Outlet");
+    const adminLayout = screen.getByText("DashboardAdminLayout");
+    expect(adminLayout).toBeInTheDocument();
+  });
 
-    expect(buttons.length).toBe(3);
-    expect(outletElement).toBeInTheDocument();
+  test("renders ManagerLayout if userRole manager", () => {
+    renderAndMock(ROLES.SHOP_MANAGER);
+
+    const managerLayout = screen.getByText("DashboardManagerLayout");
+    expect(managerLayout).toBeInTheDocument();
   });
 });
