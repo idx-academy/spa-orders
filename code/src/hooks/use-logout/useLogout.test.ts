94654d21cd53e26ca8aefde180679e51b025a7c4
Georgiy Shvab <georgiy.shvab@gmail.com>
2024-07-30 14:20:44 +0300

    Added tests for useLogout

---
+++ code/src/hooks/use-logout/useLogout.test.ts
@@ -0,0 +1,42 @@
+import { renderHook } from "@testing-library/react";
+
+import useLogout from "./useLogout";
+
+const mockDispatch = jest.fn();
+
+jest.mock("@/store/slices/userSlice", () => ({
+  logout: jest.fn()
+}));
+
+jest.mock("@/store/api/cartApi", () => ({
+  __esModule: true,
+  default: {
+    util: {
+      resetApiState: jest.fn()
+    }
+  }
+}));
+
+jest.mock("@/store/slices/localCart", () => ({
+  endpoints: {
+    getCartItems: {
+      matchFulfilled: jest.fn()
+    }
+  },
+  clearLocalCart: jest.fn()
+}));
+
+jest.mock("@/hooks/use-redux/useRedux", () => ({
+  __esModule: true,
+  useAppDispatch: () => mockDispatch
+}));
+
+describe("Test useLogout hook", () => {
+  test("Should call dispatch 3 times", () => {
+    const { result } = renderHook(() => useLogout());
+
+    result.current();
+
+    expect(mockDispatch).toHaveBeenCalledTimes(3);
+  });
+});
