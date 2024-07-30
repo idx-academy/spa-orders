9bc141b8bf5b96b2a21b8f4191575e2f8087e2c0
mhevyk <maxxevyk7@gmail.com>
2024-07-30 15:46:23 +0300

    Fix eslint error

--- code/src/components/app-range-slider/AppRangeSlider.test.tsx
+++ code/src/components/app-range-slider/AppRangeSlider.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
 import AppRangeSlider from "@/components/app-range-slider/AppRangeSlider";
 
