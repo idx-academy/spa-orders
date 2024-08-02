fce3cf323bd992c04ecf2f59af7b9b4d7970fb38
ValKoDim <v.dimitrov_1994@abv.bg>
2024-08-02 09:09:28 +0300

    Changes to skeleton footer container

--- code/src/components/product-skeleton/ProductSkeleton.tsx
+++ code/src/components/product-skeleton/ProductSkeleton.tsx
@@ -16,7 +16,7 @@ const ProductSkeleton = () => {
         width={254}
         animation="pulse"
       />
-      <AppBox className="spa-product-skeleton__footer">
+      <AppBox className="spa-product-skeleton__footer" width={234}>
         <AppSkeleton variant="text" width={64} height={35} animation="pulse" />
         <AppSkeleton
           variant="circular"
