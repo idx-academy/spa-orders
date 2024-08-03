e0621b0c56e4b6b6359e1eb2899d3ba2294dfab8
Dinviz <eugene.hetsyanyn@gmail.com>
2024-08-03 11:36:36 +0300

    Created HeaderSearchInputDropdown

---
+++ code/src/layouts/header/components/header-search-input-dropdown/HeaderSearchInputDropdown.test.tsx
@@ -0,0 +1,75 @@
+import { fireEvent, screen } from "@testing-library/react";
+
+import HeaderSearchInputDropdown from "@/layouts/header/components/header-search-input-dropdown/HeaderSearchInputDropdown";
+
+import { ProductFromSearch } from "@/types/product.types";
+import renderWithProviders from "@/utils/render-with-providers/renderWithProviders";
+
+jest.mock("@/store/api/productsApi", () => ({
+  useGetUserProductsBySearchQuery: jest.fn()
+}));
+
+const searchResults: ProductFromSearch[] = [
+  { id: "1", name: "Product 1", image: "image1.png" },
+  { id: "2", name: "Product 2", image: "image2.png" }
+];
+
+describe("HeaderSearchInputDropdown", () => {
+  const noResultsLabel = /header.searchInputNoResults/;
+  const mockHandleCloseDropdown = jest.fn();
+
+  const renderComponent = (props = {}) => {
+    const defaultProps = {
+      handleCloseDropdown: mockHandleCloseDropdown,
+      searchResults: [],
+      totalElements: 0,
+      isError: false,
+      isLoading: false,
+      ...props
+    };
+    return renderWithProviders(<HeaderSearchInputDropdown {...defaultProps} />);
+  };
+
+  test("renders error label when isError is true", () => {
+    renderComponent({ isError: true });
+
+    const errorLabel = screen.getByText(/errors.somethingWentWrong/i);
+    expect(errorLabel).toBeInTheDocument();
+  });
+
+  test("renders loading label when isLoading is true", () => {
+    renderComponent({ isLoading: true });
+
+    const loadingLabel = screen.getByText(/Loading.../i);
+    expect(loadingLabel).toBeInTheDocument();
+  });
+
+  test("renders no results label when totalElements is 0 and not loading", () => {
+    renderComponent({ totalElements: 0 });
+
+    const noResultsLabelElement = screen.getByText(noResultsLabel);
+    expect(noResultsLabelElement).toBeInTheDocument();
+
+    const noResultsImage = screen.getByAltText(/no results image/i);
+    expect(noResultsImage).toBeInTheDocument();
+  });
+
+  test("renders search results when there are results", () => {
+    renderComponent({ searchResults, totalElements: searchResults.length });
+
+    const searchResultsLabel = screen.getByText(/header.searchInputResults/i);
+    expect(searchResultsLabel).toBeInTheDocument();
+
+    const product1 = screen.getByText(/Product 1/i);
+    expect(product1).toBeInTheDocument();
+  });
+
+  test("calls handleCloseDropdown when a search result is clicked", () => {
+    renderComponent({ searchResults, totalElements: searchResults.length });
+
+    const product1 = screen.getByText(/Product 1/i);
+    fireEvent.click(product1);
+
+    expect(mockHandleCloseDropdown).toHaveBeenCalled();
+  });
+});
---
+++ code/src/layouts/header/components/header-search-input-dropdown/HeaderSearchInputDropdown.tsx
@@ -0,0 +1,114 @@
+import AppBox from "@/components/app-box/AppBox";
+import AppLink from "@/components/app-link/AppLink";
+import AppMenuItem from "@/components/app-menu-item/AppMenuItem";
+import AppTypography from "@/components/app-typography/AppTypography";
+
+import noResultsImage from "@/assets/images/search/no-results.png";
+import routePaths from "@/constants/routes";
+import { ProductFromSearch } from "@/types/product.types";
+
+import "@/layouts/header/components/header-search-input-dropdown/HeaderSearchInputDropdown.scss";
+
+type HeaderSearchInputDropdownProps = {
+  isError: boolean;
+  searchResults: ProductFromSearch[];
+  handleCloseDropdown: () => void;
+  totalElements: number;
+  isLoading: boolean;
+};
+
+const HeaderSearchInputDropdown = ({
+  handleCloseDropdown,
+  searchResults,
+  totalElements,
+  isError,
+  isLoading
+}: HeaderSearchInputDropdownProps) => {
+  const errorLabel = (
+    <AppTypography component="li" translationKey="errors.somethingWentWrong" /> // will be replaced by error layout
+  );
+
+  const searchResultsLabel = (
+    <AppMenuItem className="search-input-dropdown__results" disabled>
+      <AppTypography
+        variant="caption"
+        component="p"
+        translationKey="header.searchInputResults"
+      ></AppTypography>
+      <AppTypography
+        className="search-input-dropdown__results-value"
+        variant="caption"
+      >
+        {totalElements}
+      </AppTypography>
+    </AppMenuItem>
+  );
+
+  //Will be implemented with pagination
+  const loadingLabel = <AppTypography component="li">Loading...</AppTypography>;
+
+  const noResultsLabel = (
+    <AppBox className="search-input-dropdown__no-results">
+      <AppTypography
+        component="li"
+        className="search-input-dropdown__no-results-label"
+        fontWeight="extra-bold"
+        translationKey="header.searchInputNoResults"
+      />
+      <AppBox
+        className="search-input-dropdown__no-results-image"
+        component="img"
+        src={noResultsImage}
+        alt="no results image"
+      />
+    </AppBox>
+  );
+
+  let content;
+
+  if (isError) {
+    content = errorLabel;
+  } else if (isLoading) {
+    content = loadingLabel;
+  } else if (totalElements === 0) {
+    content = noResultsLabel;
+  } else {
+    content = (
+      <>
+        {searchResultsLabel}
+        {searchResults.map(({ id, name, image }) => (
+          <AppMenuItem
+            key={id}
+            className="search-input-dropdown__item"
+            onClick={handleCloseDropdown}
+          >
+            <AppLink
+              className="search-input-dropdown__item-container"
+              to={routePaths.productDetails.path(id)}
+            >
+              <AppBox
+                className="search-input-dropdown__item-image"
+                component="img"
+                src={image}
+                alt={name}
+              ></AppBox>
+              <AppTypography variant="caption-small">{name}</AppTypography>
+            </AppLink>
+          </AppMenuItem>
+        ))}
+      </>
+    );
+  }
+
+  return (
+    <AppBox
+      data-testid="search-dropdown"
+      component="ul"
+      className=" search-input-dropdown"
+    >
+      {content}
+    </AppBox>
+  );
+};
+
+export default HeaderSearchInputDropdown;
