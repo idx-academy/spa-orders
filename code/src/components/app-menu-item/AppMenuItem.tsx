fca6580d6706e4165f4ae6cda6417f8f41cfb22a
Dinviz <eugene.hetsyanyn@gmail.com>
2024-08-06 16:53:05 +0300

    added forwardRef to the menu item

--- code/src/components/app-menu-item/AppMenuItem.tsx
+++ code/src/components/app-menu-item/AppMenuItem.tsx
@@ -1,9 +1,15 @@
+import { forwardRef } from "react";
+
 import MenuItem from "@mui/material/MenuItem";
 
 import { AppMenuItemProps } from "@/components/app-menu-item/AppMenuItem.types";
 
-const AppMenuItem = (props: AppMenuItemProps) => {
-  return <MenuItem {...props} />;
-};
+const AppMenuItem = forwardRef<HTMLLIElement, AppMenuItemProps>(
+  (props, ref) => {
+    return <MenuItem {...props} ref={ref} />;
+  }
+);
+
+AppMenuItem.displayName = "AppMenuItem";
 
 export default AppMenuItem;
--- code/src/components/app-menu-item/AppMenuItem.types.ts
+++ code/src/components/app-menu-item/AppMenuItem.types.ts
@@ -1,3 +1,7 @@
+import { ForwardedRef } from "react";
+
 import { MenuItemProps } from "@mui/material/MenuItem";
 
-export type AppMenuItemProps = MenuItemProps;
+export type AppMenuItemProps = MenuItemProps & {
+  ref?: ForwardedRef<HTMLLIElement>;
+};
