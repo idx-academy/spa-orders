25c9262e5cc3832d760c02cc5e7ac1578fc5931b
OliaHrytsak <oliagrytsak1991@gmail.com>
2024-08-07 17:15:57 +0300

    test changes

--- code/src/containers/dashboard-orders-filter-drawer/DashboardOrdersFilterDrawer.test.tsx
+++ code/src/containers/dashboard-orders-filter-drawer/DashboardOrdersFilterDrawer.test.tsx
@@ -1,10 +1,7 @@
 import { act, fireEvent, screen, waitFor } from "@testing-library/react";
 
 import DashboardOrdersFilterDrawer from "@/containers/dashboard-orders-filter-drawer/DashboardOrdersFilterDrawer";
-import {
-  AdminOrderFilters,
-  AdminOrderSearch
-} from "@/containers/dashboard-orders-filter-drawer/hooks/use-filtered-admin-orders/useFilteredAdminOrders.types";
+import { AdminOrderFilters } from "@/containers/dashboard-orders-filter-drawer/hooks/use-filtered-admin-orders/useFilteredAdminOrders.types";
 
 import { deliveryMethods } from "@/constants/deliveryMethods";
 import { orderStatusesTranslationKeys } from "@/constants/orderStatuses";
