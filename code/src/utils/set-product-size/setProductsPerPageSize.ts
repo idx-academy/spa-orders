ff0b0fba9945a2a19090c09ebe233e8f614ac97d
ValKoDim <v.dimitrov_1994@abv.bg>
2024-08-08 12:28:47 +0300

    Fixed comments

--- code/src/utils/set-product-size/setProductsPerPageSize.ts
+++ code/src/utils/set-product-size/setProductsPerPageSize.ts
@@ -1,14 +1,15 @@
 import { BREAKPOINTS } from "@/constants/breakpoints";
 
 const setProductsPerPageSize = (screenWidth: number) => {
-  if (screenWidth >= BREAKPOINTS.xl) {
-    return 10;
-  } else if (screenWidth >= BREAKPOINTS.l) {
-    return 12;
-  } else if (screenWidth >= BREAKPOINTS.m) {
-    return 9;
-  } else {
-    return 10;
+  switch (true) {
+    case screenWidth >= BREAKPOINTS.xl:
+      return 10;
+    case screenWidth >= BREAKPOINTS.l:
+      return 12;
+    case screenWidth >= BREAKPOINTS.m:
+      return 9;
+    default:
+      return 10;
   }
 };
 
